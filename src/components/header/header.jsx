b1f9bc1f7d0348cc0361a7ba5944b4a1357f6e87
khoi <zluffyz2002@gmail.com>
2023-05-31 10:48:54 +0700

    reponsive

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 import { Link } from "react-router-dom";
+import { FiMenu } from "react-icons/fi";
 
 const Header = () => {
+  const [menuRepon, setMenuRepon] = useState(false);
+  const handleChangeMenu = () => {
+    setMenuRepon(!menuRepon);
+  };
   return (
     <>
       <div className="header">
-        <div >
+        <div>
           <Link className="d-flex align-items-center ps-4" to={"/"}>
-          <img className="logo_page" src="img/tiktok_logo_icon.png" alt="" />
-          <div className="name_page">TikTokNoWatermark</div>
+            <img className="logo_page" src="img/tiktok_logo_icon.png" alt="" />
+            <div className="name_page">TikTokNoWatermark</div>
           </Link>
         </div>
-        <div className="m-auto">
+        <div className="m-auto position-relative">
           <ul className="d-flex  align-items-center">
             <li className="pure-menu-item">
               <Link to={"/"} className="pure-menu-link">
@@ -25,14 +30,29 @@ const Header = () => {
               </Link>
             </li>
           </ul>
+          <div className="menu_reponsive openbtn" onClick={handleChangeMenu}>
+            <FiMenu />
+          </div>
+          {menuRepon ? (
+            <div id="mySidepanel" className="sidepanel">
+              <div className="pt-4 pb-2">
+                <Link to={"/"} className="pure-menu-link">
+                  Main function.
+                </Link>
+              </div>
+              <div className="pt-2 pb-4">
+                <Link to={"/downloadApp"} className="pure-menu-link">
+                  Download App
+                </Link>
+              </div>
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
         <div className="standforukraine">
-          <div className="cl__1">
-
-          </div>
-          <div className="cl__2">
-
-          </div>
+          <div className="cl__1"></div>
+          <div className="cl__2"></div>
         </div>
       </div>
     </>
--- src/pages/downloadApp/downloadApp.jsx
+++ src/pages/downloadApp/downloadApp.jsx
@@ -38,77 +38,80 @@ const DownloadApp = () => {
                   </div>
                 </div>
               </section>
-              <div className="blue__block">
-                <ol>
-                  <li>
-                    <strong>
-                      <span>
-                        Download the app in the Download App box above
-                      </span>
-                    </strong>
-                    <p>After downloading the app we come to the next step</p>
-                  </li>
-                  <li>
-                    <strong>
-                      <span>
-                        Because of privacy policy we will have a key that only
-                        When you have it, you can run the App (Contact Admin)
-                      </span>
-                    </strong>
-                    <p>
-                      enter the key code in the first input box when opening the
-                      app then validate key
-                    </p>
-                  </li>
-                  <li>
-                    <strong>
-                      <span>
-                        Find the TikTok Username or keywords you want to
-                        download
-                      </span>
-                    </strong>
-                    <p>
-                      Open the TikTok app and find the account you want to save
-                      under in MP4, MP4 format and then click on that account
-                      You will see the box “sidebar” at the top of the screen.
-                      Tap it then press enter “Username” on the screen as
-                      “tiktok” or “@tiktok”.
-                      <br />
-                      As for keywords, you just need to enter keywords into the
-                      app to download .
-                    </p>
-                  </li>
-                  <li>
-                    <strong>
-                      <span>Paste Username , keywords in App</span>
-                    </strong>
-                    <p>
-                      You can use our TikTok MP4  downloader me on any page
-                      of the App. TikTok video download to MP4 completely free
-                      and in seconds!
-                    </p>
-                    <p>
-                      Almost done. Now press the Download button and select the
-                      file to save the video in your computer and then download
-                      it TikTok video in MP4 format and wait for the result
-                    </p>
-                  </li>
-                  <li>
-                    <strong>
-                      <span>Download TikTok videos</span>
-                    </strong>
-                    <p>
-                      If everything is fine, the "Results" page will open.
-                      Scroll down to see the "Download MP4" link at the bottom
-                      of the page
-                    </p>
-                    <p>
-                      Sometimes you won't be able to see a video. This has means
-                      that the video is not available for TikTok. We are solve
-                      this problem and it will be fixed soon.
-                    </p>
-                  </li>
-                </ol>
+              <div className="ps-5 pe-5">
+                <div className="blue__block">
+                  <ol>
+                    <li>
+                      <strong>
+                        <span>
+                          Download the app in the Download App box above
+                        </span>
+                      </strong>
+                      <p>After downloading the app we come to the next step</p>
+                    </li>
+                    <li>
+                      <strong>
+                        <span>
+                          Because of privacy policy we will have a key that only
+                          When you have it, you can run the App (Contact Admin)
+                        </span>
+                      </strong>
+                      <p>
+                        enter the key code in the first input box when opening
+                        the app then validate key
+                      </p>
+                    </li>
+                    <li>
+                      <strong>
+                        <span>
+                          Find the TikTok Username or keywords you want to
+                          download
+                        </span>
+                      </strong>
+                      <p>
+                        Open the TikTok app and find the account you want to
+                        save under in MP4, MP4 format and then click on that
+                        account You will see the box “sidebar” at the top of the
+                        screen. Tap it then press enter “Username” on the screen
+                        as “tiktok” or “@tiktok”.
+                        <br />
+                        As for keywords, you just need to enter keywords into
+                        the app to download .
+                      </p>
+                    </li>
+                    <li>
+                      <strong>
+                        <span>Paste Username , keywords in App</span>
+                      </strong>
+                      <p>
+                        You can use our TikTok MP4 downloader me on any page of
+                        the App. TikTok video download to MP4 completely free
+                        and in seconds!
+                      </p>
+                      <p>
+                        Almost done. Now press the Download button and select
+                        the file to save the video in your computer and then
+                        download it TikTok video in MP4 format and wait for the
+                        result
+                      </p>
+                    </li>
+                    <li>
+                      <strong>
+                        <span>Download TikTok videos</span>
+                      </strong>
+                      <p>
+                        If everything is fine, the "Results" page will open.
+                        Scroll down to see the "Download MP4" link at the bottom
+                        of the page
+                      </p>
+                      <p>
+                        Sometimes you won't be able to see a video. This has
+                        means that the video is not available for TikTok. We are
+                        solve this problem and it will be fixed soon.
+                      </p>
+                    </li>
+                  </ol>
+                </div>
               </div>
             </div>
           </div>
