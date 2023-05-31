d12eab916768224c8e3e55c43fc6fc3daba35e94
khoi <zluffyz2002@gmail.com>
2023-05-31 16:16:11 +0700

    change text

--- src/pages/contacts/contacts.jsx
+++ src/pages/contacts/contacts.jsx
@@ -13,12 +13,15 @@ const Contacts = () => {
         <div className="content-wrapper text-center">
           <div className="content main__content__container">
             <div className="content u-mtop">
-              <h1>Liên hệ với chúng tôi - Gmail</h1>
+              <h1>Contact us - Gmail</h1>
               <br />
               <h3>zluffyz2002@gmail.com</h3>
               <hr className="w-75 m-auto" />
               <p className="w-75 m-auto pt-5">
-              Chúng tôi ở đây để trả lời bất kỳ câu hỏi hoặc yêu cầu mà bạn có thể có. Hãy liên hệ với chúng tôi và chúng tôi sẽ trả lời ngay khi có thể. Nhìn thấy một lỗi? Có gì đó không ổn với video? Sử dụng mẫu để báo cáo!
+                We are here to answer any questions or requests you have may
+                have. Contact us and we'll respond right away when possible. See
+                an error? Is there something wrong with the video? History Use
+                the form to report!
               </p>
             </div>
           </div>
