6621741ea59e90ca93e128030f0902a1bda829f4
khoi <zluffyz2002@gmail.com>
2023-05-31 17:07:59 +0700

    update btn__downlaod

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -1,36 +1,35 @@
 import React from "react";
 import "./banner.css";
-import { Link } from "react-router-dom";
 
-const ZIP_FIlE_URL = "http://localhost:3000/tiktok-win32-x64.zip";
+// const ZIP_FIlE_URL = "http://localhost:3000/tiktok-win32-x64.zip";
 const Banner = () => {
-  const downloadFileAtUrl = (url) => {
-    fetch(url)
-      .then((Response) => Response.blob())
-      .then((blob) => {
-        const blobURL = window.URL.createObjectURL(new Blob([blob]));
-        console.log(blobURL, "bloURL");
-      });
-    const fileName = url.split("/").pop();
-    const aTag = document.createElement("a");
-    aTag.href = url;
-    aTag.setAttribute("download", fileName);
-    document.body.appendChild(aTag);
-    aTag.click();
-    aTag.remove();
-  };
+  // const downloadFileAtUrl = (url) => {
+  //   fetch(url)
+  //     .then((Response) => Response.blob())
+  //     .then((blob) => {
+  //       const blobURL = window.URL.createObjectURL(new Blob([blob]));
+  //       console.log(blobURL, "bloURL");
+  //     });
+  //   const fileName = url.split("/").pop();
+  //   const aTag = document.createElement("a");
+  //   aTag.href = url;
+  //   aTag.setAttribute("download", fileName);
+  //   document.body.appendChild(aTag);
+  //   aTag.click();
+  //   aTag.remove();
+  // };
   return (
     <>
       <div className="banner">
         <h1 className="bigmessage">Download TikTok MP4</h1>
         <div className="text-center">
-          <Link
-            className="btn__download text-dark bg-white"
+          <a
+            className="btn__download "
             href="https://github.com/khoilion/WebDownLoadTikTokNoWatermark/releases/download/v1.0.0/tiktok-win32-x64.zip"
         
           >
             Download App
-          </Link>
+          </a>
         </div>
       </div>
     </>
